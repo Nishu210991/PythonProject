e3f210b063ae7bcecb86f3f1532660b3314174a5
Nishu <scorpions.group@gmail.com>
2021-02-18 15:43:48 +0530

    18.1 Commit

--- First.py
+++ First.py
@@ -1,6 +1,12 @@
 print("Nisshuuu..")
 print("Hello")
-print("Hiiiiii")
-print("Running")
-print("Running")
-print("Running")
+L = ['red', 'green', 'blue', 'yellow', 'black']
+print(L[-1])
+
+L = ['a', 'b', ['cc', 'dd', ['eee', 'fff']], 'g', 'h']
+
+print(L[2][1])
+print(L[2][2][1])
+
+L = ['a', 'b', 'c', 'd', 'e', 'f']
+print(L[1:-1])
