11530d591213d7d1d46549baf4322e4e7276b633
Nishu <scorpions.group@gmail.com>
2021-02-24 14:12:05 +0530

    24.Snake_Game Commit

--- snake_water_gun.py
+++ snake_water_gun.py
@@ -2,49 +2,54 @@ import random
 i=1
 count_comp=0
 count_user=0
+chance=6
 
 while i<=6:
     game_pointer = ['S', 'W', 'G']
     rand = random.choice(game_pointer)
     #print(rand)
     input1 = input("Enter first key word of yours choice:S, W, G")
+    if chance>0:
 
+        if input1=='S' and rand=='G':
+            print("Oooopppsss Gun shoot to your Snake","Your Choice:" ,input1,"Computer Choice:", rand)
+            count_comp = count_comp + 1
+            print("You Loss",count_comp)
 
-    if input1=='S' and rand=='G':
-        print("Oooopppsss Gun shoot to your Snake","Your Choice:" ,input1,"Computer Choice:", rand)
-        count_comp = count_comp + 1
-        print("You Loss",count_comp)
+        elif input1=='S' and rand=='W':
+            print("Wooo!!!You won your snake drunk water ", "Your Choice:", input1, "Computer Choice:", rand)
+            count_user = count_user + 1
+            print("You won", count_user)
 
-    elif input1=='S' and rand=='W':
-        print("Wooo!!!You won your snake drunk water ", "Your Choice:", input1, "Computer Choice:", rand)
-        count_user = count_user + 1
-        print("You won", count_user)
+        elif input1=='W' and rand=='G':
+            print("Wooo! You won gun drown into the water", "Your Choice:", input1, "Computer Choice:", rand)
+            count_user = count_user + 1
+            print("You won", count_user)
 
-    elif input1=='W' and rand=='G':
-        print("Wooo! You won gun drown into the water", "Your Choice:", input1, "Computer Choice:", rand)
-        count_user = count_user + 1
-        print("You won", count_user)
+        elif input1=='W' and rand=='S':
+            print("Oooopppsss You Loss snake drunk your all water", "Your Choice:", input1, "Computer Choice:", rand)
+            count_comp = count_comp + 1
+            print("You Loss", count_comp)
 
-    elif input1=='W' and rand=='S':
-        print("Oooopppsss You Loss snake drunk your all water", "Your Choice:", input1, "Computer Choice:", rand)
-        count_comp = count_comp + 1
-        print("You Loss", count_comp)
+        elif input1=='G' and rand=='W':
+            print("Oooopppss!! Your Gun drown into the water", "Your Choice:", input1, "Computer Choice:", rand)
+            count_comp = count_comp + 1
+            print("You Loss", count_comp)
 
-    elif input1=='G' and rand=='W':
-        print("Oooopppss!! Your Gun drown into the water", "Your Choice:", input1, "Computer Choice:", rand)
-        count_comp = count_comp + 1
-        print("You Loss", count_comp)
+        elif input1 == 'G' and rand == 'S':
+            print("Wooo!!! You won Your Gun shoot snake", "Your Choice:", input1, "Computer Choice:", rand)
+            count_user = count_user + 1
+            print("You Won", count_user)
 
-    elif input1 == 'G' and rand == 'S':
-        print("Wooo!!! You won Your Gun shoot snake", "Your Choice:", input1, "Computer Choice:", rand)
-        count_user = count_user + 1
-        print("You Won", count_user)
+        elif input1==rand:
+            print("Match Tie:Both Guesses are same")
 
-    elif input1==rand:
-        print("Match Tie:Both Guesses are same")
+        elif input1!=rand:
+            print("Please Enter Valid Keyword")
 
-    i=i+1
-
+        chance=chance-1
+        print("Left Chances:", chance)
+    i = i + 1
 print("Your Game Limit Exceeded\n","You Won:",count_user ,"Computer Won:", count_comp)
 
 
