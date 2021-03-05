95b4c9b6c8a5d4ed8b1e84351d3944451ab73a4e
Nishu <scorpions.group@gmail.com>
2021-03-05 13:54:13 +0530

    05.03_Join_func Commit

---
+++ Join_func.py
@@ -0,0 +1,10 @@
+# Join Function
+l1 = ["NIshu", "Dikshu", "Anupam", "Golu", "Vishvajeet"]
+
+#Normal
+# for item in l1:
+#     #print(item, "and" ,end=" ") #join with and
+#     print(item, ",", end=" ")  #join with comma
+
+a= " and ".join(l1)
+print(a, "All are Thakur")
