d4518873e8d4ebbb994f7e9b710e2ef754656af2
Nishu <scorpions.group@gmail.com>
2021-03-07 12:23:25 +0530

    07.03_OOPs_mullevel_Inherit Commit

--- MultiLevel_Inherit.py
+++ MultiLevel_Inherit.py
@@ -1,8 +1,23 @@
 class Dad:
-    pass
+    Badmitton=5
 
 class Son(Dad):
-    pass
+    Dance=2
+    #Badmitton = 10
+    def isdance(self):
+        return f"Yes I can dance {self.Dance} anytime."
 
 class GrandSon(Son):
-    pass
+     Dance = 10
+     #Badmitton = 15   # 1st search in its own class(Grandson) then Son then  Dad
+     def isdance(self):
+         return f"Yeah! I can dance {self.Dance} like Madhuri Dixit."
+
+
+Dada=Dad()
+Beta=Son()
+Pota=GrandSon()
+
+print(Pota.isdance())
+print(Beta.isdance())
+print(Pota.Badmitton)
