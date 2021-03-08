200aea676055fcb7c2f854a143470482e7b07420
Nishu <scorpions.group@gmail.com>
2021-03-08 17:29:26 +0530

    08.03_Else_with-ForLoop Commit

---
+++ ElsewithForloop.py
@@ -0,0 +1,14 @@
+Games = ["Kho-kho", "Badmitton", "Hockey", "Bollyball"]
+# for item in Games:
+#     print(item)
+#     #break  #print only 1 item and break it
+# else:
+#     print("Games is over")
+
+# With If condiition
+
+for item in Games:
+    if item == "Cricket":
+       break
+else:
+    print("Games is not found in the List")
